75728315130562cc1187438bf2480a6c43752709
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2021-09-18 08:41:57 +0530

    Revert "timepass test"
    This reverts commit 443cfa5cf9733205f59ca98af3ab79999ef8704f.

--- device.mk
+++ device.mk
@@ -50,7 +50,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti.recovery \
-	android.hardware.boot@1.1-impl-qti \
     android.hardware.boot@1.1-service
 
 PRODUCT_PACKAGES += \
