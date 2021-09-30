34c020300de1f0d8235b3d6384aa3d60df76b8d1
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2021-09-30 23:16:30 +0530

    drone.yml latest

--- build.sh
+++ build.sh
@@ -24,8 +24,8 @@ git clone --depth=1 $DT_LINK $DT_PATH
 echo " ===+++ Building Recovery +++==="
 cd bootable/recovery
 curl -sL https://github.com/TeamWin/android_bootable_recovery/commit/22851b9476be92b6718baf6fb51eeefa9e2e6d0b.patch | patch -R -p1
-cd..
-cd..
+cd ..
+cd ..
 
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
