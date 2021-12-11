00c6ab1ad45bf502a71845443114e10dc37360ff
xyen8180 <88905324+xyen8180@users.noreply.github.com>
2021-12-10 22:34:37 -0800

    fix

--- device.mk
+++ device.mk
@@ -27,6 +27,9 @@ PRODUCT_TARGET_VNDK_VERSION := 30
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # A/B
 AB_OTA_UPDATER := true
